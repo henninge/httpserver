c611436c4ea6c5e182ae5b2e039b25dd63e84e28
Henning Eggers <henning@keeeb.com>
2017-11-19 15:15:14 +0100

    Use nio.Files.

--- src/DirectoryResponse.java
+++ src/DirectoryResponse.java
@@ -1,9 +1,10 @@
 package httpserver;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 
 public class DirectoryResponse extends HttpResponse {
@@ -27,9 +28,12 @@ public class DirectoryResponse extends HttpResponse {
 
         writer.println(String.format(
             "<html><body><h1>%1s</h2><ul>", getHref(directory)));
-        for (File file: directory.toFile().listFiles()) {
-            writer.println(String.format(
-                "<li><a href=\"%1s\">%2s</a></li>", getHref(file.toPath()), file.getName()));
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+           for (Path file: stream) {
+                writer.println(String.format(
+                    "<li><a href=\"%1s\">%2s</a></li>", getHref(file), file.getFileName()));
+           }
         }
         writer.println("</ul></body></html>");
     }
